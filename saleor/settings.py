19423365a7fc6369d7c06c8c8d97fc781074faa9
manzzup <manujith.nc@gmail.com>
2020-12-24 15:54:34 +0530

    added avantrio host

--- saleor/settings.py
+++ saleor/settings.py
@@ -46,7 +46,7 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1"
+_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1,apps.avantrio.xyz"
 
 ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
 if not ALLOWED_CLIENT_HOSTS:
