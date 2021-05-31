1e056e747e89507ea51d27f530e4f78a5930f1f3
manzzup <manujith.nc@gmail.com>
2021-05-31 22:35:09 +0530

    remove email confirmation and show unconfirmed orders

--- saleor/graphql/account/types.py
+++ saleor/graphql/account/types.py
@@ -335,7 +335,7 @@ class User(CountableDjangoObjectType):
         viewer = info.context.user
         if viewer.has_perm(OrderPermissions.MANAGE_ORDERS):
             return root.orders.all()  # type: ignore
-        return root.orders.confirmed()  # type: ignore
+        return root.orders.all()  # type: ignore
 
     @staticmethod
     def resolve_avatar(root: models.User, info, size=None, **_kwargs):
